34f2a7b942c25565d0a585249545a8875b0e0b71
theeMahmud <mahmudrajucse@gmail.com>
2021-07-03 12:22:30 +0600

    sso add

--- routes/web.php
+++ routes/web.php
@@ -13,67 +13,71 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('loginPage');
-});
-Route::get('/dashboard', function () {
-    return view('pages.aside');
-});
-Route::get('/list-view', function () {
-    return view('pages.listView');
-});
-Route::get('/full-width', function () {
-    return view('pages.fullWidth');
-});
-Route::get('/profile', function () {
-    return view('pages.profile.profileInformation');
-});
-Route::get('/change-password', function () {
-    return view('pages.profile.changePassword');
-});
-Route::get('/work-history', function () {
-    return view('pages.profile.workHistory');
-});
-Route::get('/change-profile-image', function () {
-    return view('pages.profile.changeProfileImage');
-});
-Route::get('/change-signature', function () {
-    return view('pages.profile.changeSignature');
-});
-Route::get('/notification-config', function () {
-    return view('pages.profile.notification');
-});
+Route::group(['middleware' => 'jisf.auth'], function () {
+    Route::get('/dashboard', function () {
+        return view('pages.aside');
+    });
+    Route::get('/list-view', function () {
+        return view('pages.listView');
+    });
+    Route::get('/full-width', function () {
+        return view('pages.fullWidth');
+    });
+    Route::get('/profile', function () {
+        return view('pages.profile.profileInformation');
+    });
+    Route::get('/change-password', function () {
+        return view('pages.profile.changePassword');
+    });
+    Route::get('/work-history', function () {
+        return view('pages.profile.workHistory');
+    });
+    Route::get('/change-profile-image', function () {
+        return view('pages.profile.changeProfileImage');
+    });
+    Route::get('/change-signature', function () {
+        return view('pages.profile.changeSignature');
+    });
+    Route::get('/notification-config', function () {
+        return view('pages.profile.notification');
+    });
 
-Route::get('/list-view', function () {
-    return view('pages.listView');
-});
+    Route::get('/list-view', function () {
+        return view('pages.listView');
+    });
 
-Route::get('/buttons', function () {
-    return view('pages.buttons');
-});
+    Route::get('/buttons', function () {
+        return view('pages.buttons');
+    });
 
-Route::get('/file-upload', function () {
-    return view('pages.fileUpload');
-});
+    Route::get('/file-upload', function () {
+        return view('pages.fileUpload');
+    });
 
-Route::get('/image-crop', function () {
-    return view('pages.imageCrop');
-});
+    Route::get('/image-crop', function () {
+        return view('pages.imageCrop');
+    });
 
-Route::get('/tree-view', function () {
-    return view('pages.treeView');
-});
+    Route::get('/tree-view', function () {
+        return view('pages.treeView');
+    });
 
-Route::get('/drug-and-drop', function () {
-    return view('pages.dragAndDrop');
-});
+    Route::get('/drug-and-drop', function () {
+        return view('pages.dragAndDrop');
+    });
 
-Route::get('/forms', function () {
-    return view('pages.forms');
-});
+    Route::get('/forms', function () {
+        return view('pages.forms');
+    });
 
-Route::get('/ajax-load', function () {
+    Route::get('/ajax-load', function () {
         return 'এজাক্স লোড ডাটা';
+    });
 });
+
+Route::get('/', function () {
+    return redirect('/dashboard');
+});
+
 
 // Route::get('/ajax-load', 'ajaxLoadPageController@getTemplate');
