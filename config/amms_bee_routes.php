bdd71840b9bea1b1ed5c61094e6a8cbfeb26afa6
theeMahmud <mahmudrajucse@gmail.com>
2021-07-09 19:18:21 +0600

    audit_operational_plan

--- config/amms_bee_routes.php
+++ config/amms_bee_routes.php
@@ -36,5 +36,24 @@ return [
         'op_activity_show' => env('API_URL_BEE', '') . '/x-operational-plan/activity/show',
         'op_activity_update' => env('API_URL_BEE', '') . '/x-operational-plan/activity/update',
         'op_activity_delete' => env('API_URL_BEE', '') . '/x-operational-plan/activity/delete',
-    ]
+    ],
+    'audit_operational_plan' => [
+        'op_yearly_lists' => env('API_URL_BEE', '') . '/x-operational-plan/yearly',
+        'op_yearly_create' => env('API_URL_BEE', '') . '/x-operational-plan/yearly/create',
+        'op_yearly_show' => env('API_URL_BEE', '') . '/x-operational-plan/yearly/show',
+        'op_yearly_update' => env('API_URL_BEE', '') . '/x-operational-plan/yearly/update',
+        'op_yearly_delete' => env('API_URL_BEE', '') . '/x-operational-plan/yearly/delete',
+
+        'op_activity_lists' => env('API_URL_BEE', '') . '/x-operational-plan/activity',
+        'op_activity_create' => env('API_URL_BEE', '') . '/x-operational-plan/activity/create',
+        'op_activity_show' => env('API_URL_BEE', '') . '/x-operational-plan/activity/show',
+        'op_activity_update' => env('API_URL_BEE', '') . '/x-operational-plan/activity/update',
+        'op_activity_delete' => env('API_URL_BEE', '') . '/x-operational-plan/activity/delete',
+
+        'op_activity_milestone_lists' => env('API_URL_BEE', '') . '/x-operational-plan/activity-milestone',
+        'op_activity_milestone_create' => env('API_URL_BEE', '') . '/x-operational-plan/activity-milestone/create',
+        'op_activity_milestone_show' => env('API_URL_BEE', '') . '/x-operational-plan/activity-milestone/show',
+        'op_activity_milestone_update' => env('API_URL_BEE', '') . '/x-operational-plan/activity-milestone/update',
+        'op_activity_milestone_delete' => env('API_URL_BEE', '') . '/x-operational-plan/activity-milestone/delete',
+    ],
 ];
