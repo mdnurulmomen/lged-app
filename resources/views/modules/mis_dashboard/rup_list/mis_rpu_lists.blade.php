9f4267ca6a299230f8132d9d183a0dc733340ea7
Robin <faisalrobin22@gmail.com>
2021-10-04 00:18:49 +0600

    mis report

--- resources/views/modules/mis_dashboard/rup_list/mis_rpu_lists.blade.php
+++ resources/views/modules/mis_dashboard/rup_list/mis_rpu_lists.blade.php
@@ -1,48 +1,56 @@
 <x-title-wrapper>Rpu List</x-title-wrapper>
-<form class="pl-4 pt-4">
-    <div class="form-row">
-        <div class="col-md-6">
-            <label>Directorate</label>
-            <select class="form-control select-select2" name="directorate_id" id="directorate_id"
-                    onchange="MIS_RPU_LIST_CONTAINER.loadMinistryLists()">
-                <option value="">Choose Directorates</option>
-                @foreach($directorates as $directorate)
-                    <option value="{{$directorate['office_id']}}">{{$directorate['office_name_en']}}</option>
-                @endforeach
-            </select>
-        </div>
 
-        <div class="col-md-6">
-            <label>Ministry</label>
-            <select class="form-control select-select2" name="office_ministry_id" id="office_ministry_id">
-                <option value="">Choose Ministry</option>
-            </select>
-        </div>
+<div class="row">
+    <div class="col-md-12">
+        <form class="pl-4 pt-4">
+            <div class="form-row">
+                <div class="col-md-6">
+                    <label>Directorate</label>
+                    <select class="form-control select-select2" name="directorate_id" id="directorate_id"
+                            onchange="MIS_RPU_LIST_CONTAINER.loadMinistryLists()">
+                        <option value="">Choose Directorates</option>
+                        @foreach($directorates as $directorate)
+                            <option value="{{$directorate['office_id']}}">{{$directorate['office_name_en']}}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="col-md-6">
+                    <label>Ministry</label>
+                    <select class="form-control select-select2" name="office_ministry_id" id="office_ministry_id">
+                        <option value="">Choose Ministry</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <label>Audit Due Year</label>
+                    <input type="text" class="form-control" placeholder="Audit Due Year" id="audit_due_year"
+                           name="audit_due_year">
+                </div>
+
+                <div class="col-md-6">
+                    <label>Risk Area </label>
+                    <select class="form-control select-select2" name="risk_category" id="risk_category">
+                        <option value="">Choose Risk Area</option>
+                        <option value="high">High</option>
+                        <option value="medium">Medium</option>
+                        <option value="low">Low</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-md-6">
+                    <button type="button" onclick="MIS_RPU_LIST_CONTAINER.loadRupLists()" class="btn btn-primary">Submit
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
-
-    <div class="row">
-        <div class="col-md-6">
-            <label>Audit Due Year</label>
-            <input type="text" class="form-control" placeholder="Audit Due Year" id="audit_due_year" name="audit_due_year">
-        </div>
-
-        <div class="col-md-6">
-            <label>Risk Area </label>
-            <select class="form-control select-select2" name="risk_category" id="risk_category">
-                <option value="">Choose Risk Area </option>
-                <option value="high">High</option>
-                <option value="medium">Medium</option>
-                <option value="low">Low</option>
-            </select>
-        </div>
-    </div>
-
-    <div class="row">
-        <button type="button" onclick="MIS_RPU_LIST_CONTAINER.loadRupLists()" class="btn btn-primary">Submit</button>
-    </div>
-</form>
-
-<div class="px-3 py-3" id="load_mis_team_lists" >
+</div>
+<div class="px-3 py-3" id="load_mis_team_lists">
 
 </div>
 
@@ -64,7 +72,7 @@
             office_ministry_id = $('#office_ministry_id').val();
             audit_due_year = $('#audit_due_year').val();
             risk_category = $('#risk_category').val();
-            let data = {directorate_id,office_ministry_id,audit_due_year,risk_category};
+            let data = {directorate_id, office_ministry_id, audit_due_year, risk_category};
             ajaxCallAsyncCallbackAPI(url, data, 'POST', function (response) {
                 $('#load_mis_team_lists').html(response);
             });
