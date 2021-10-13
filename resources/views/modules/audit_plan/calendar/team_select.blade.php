12711ec5ad5b4d593ca4c88ab5296c8189515900
Robin <faisalrobin22@gmail.com>
2021-10-13 15:52:13 +0600

    add team leader in calendar

--- resources/views/modules/audit_plan/calendar/team_select.blade.php
+++ resources/views/modules/audit_plan/calendar/team_select.blade.php
@@ -1,4 +1,4 @@
 <option value="">All Teams</option>
 @foreach($team_list as $team)
-     <option value="{{$team['id']}}">{{$team['team_name']}}</option>
+     <option value="{{$team['id']}}">{{$team['team_name']}} ({{$team['leader_name_bn']}})</option>
 @endforeach
