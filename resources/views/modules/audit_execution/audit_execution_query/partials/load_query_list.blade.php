64576b6608bffe631be278ba7b3e0f601d4412ce
theeMahmud <mahmudrajucse@gmail.com>
2021-10-30 23:28:47 +0600

    h1

--- resources/views/modules/audit_execution/audit_execution_query/partials/load_query_list.blade.php
+++ resources/views/modules/audit_execution/audit_execution_query/partials/load_query_list.blade.php
@@ -39,7 +39,7 @@
                 @if($query['audit_query'] && $query['audit_query']['status'] != 'removed')
                     @if($query['audit_query']['is_query_document_received'] == 0)
                         <button onclick="Audit_Query_Container.receivedQuery('{{$query['id']}}')"
-                            type="button" title="receive"
+                            type="button" title="Receive"
                             class="btn btn-icon btn-square btn-sm btn-light btn-hover-icon-primary btn-icon-primary receivedQuery">
                             <i class="fas fa-check-double"></i>
                         </button>
