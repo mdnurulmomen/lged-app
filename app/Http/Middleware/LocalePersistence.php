421880245eedd5339f27df764427553a1e4740f2
theeMahmud <mahmudrajucse@gmail.com>
2021-11-08 19:41:28 +0600

    lang reinforced

---
+++ app/Http/Middleware/LocalePersistence.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Session;
+
+class LocalePersistence
+{
+    public function handle($request, Closure $next)
+    {
+        if (Session::has('locale') && array_key_exists(Session::get('locale'), Config::get('cag_amms_config.lang'))) {
+            App::setLocale(Session::get('locale'));
+        } else { // This is optional as Laravel will automatically set the fallback language if there is none specified
+            App::setLocale(Config::get('app.fallback_locale'));
+        }
+        return $next($request);
+    }
+}
