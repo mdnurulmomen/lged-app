47dfcec652d91de0ade5ce931d2329f90c76af41
theeMahmud <mahmudrajucse@gmail.com>
2021-11-09 12:30:40 +0600

    locale

--- app/Http/Middleware/LocalePersistence.php
+++ app/Http/Middleware/LocalePersistence.php
@@ -11,7 +11,8 @@ class LocalePersistence
 {
     public function handle($request, Closure $next)
     {
-        if (Session::has('locale') && array_key_exists(Session::get('locale'), Config::get('cag_amms_config.lang'))) {
+        if (Session::has('locale')) {
+//        if (Session::has('locale') && array_key_exists(Session::get('locale'), Config::get('cag_amms_config.lang'))) {
             App::setLocale(Session::get('locale'));
         } else { // This is optional as Laravel will automatically set the fallback language if there is none specified
             App::setLocale(Config::get('app.fallback_locale'));
