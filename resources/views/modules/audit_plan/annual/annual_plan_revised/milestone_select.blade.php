deb4cc414763d61c7eee05bbad756ff1cc5bc3fc
Robin <faisalrobin22@gmail.com>
2021-11-11 18:42:08 +0600

    select milestone

--- resources/views/modules/audit_plan/annual/annual_plan_revised/milestone_select.blade.php
+++ resources/views/modules/audit_plan/annual/annual_plan_revised/milestone_select.blade.php
@@ -1,3 +1,4 @@
 @foreach($all_milestone as $milestone)
+    <option value="">মাইলস্টোন বাছাই করুন</option>
     <option value="{{$milestone['id']}}">{{$milestone['title_bn']}} </option>
 @endforeach
