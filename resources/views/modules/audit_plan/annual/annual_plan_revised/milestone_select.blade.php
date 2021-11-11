4457f28bd255df796ad5a1fe1d7f1a3a93cbf481
Robin <faisalrobin22@gmail.com>
2021-11-11 18:43:27 +0600

    milestone change

--- resources/views/modules/audit_plan/annual/annual_plan_revised/milestone_select.blade.php
+++ resources/views/modules/audit_plan/annual/annual_plan_revised/milestone_select.blade.php
@@ -1,4 +1,4 @@
+<option value="">মাইলস্টোন বাছাই করুন</option>
 @foreach($all_milestone as $milestone)
-    <option value="">মাইলস্টোন বাছাই করুন</option>
     <option value="{{$milestone['id']}}">{{$milestone['title_bn']}} </option>
 @endforeach
