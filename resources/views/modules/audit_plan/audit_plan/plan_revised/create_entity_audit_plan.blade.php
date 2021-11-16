b9c6ff617cfdbec28f26c3296524f62121d7fe58
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2021-11-16 17:58:48 +0600

    audit plan book preview width increase

--- resources/views/modules/audit_plan/audit_plan/plan_revised/create_entity_audit_plan.blade.php
+++ resources/views/modules/audit_plan/audit_plan/plan_revised/create_entity_audit_plan.blade.php
@@ -162,7 +162,7 @@
                         quick_panel = $("#kt_quick_panel");
                         quick_panel.addClass('offcanvas-on');
                         quick_panel.css('opacity', 1);
-                        quick_panel.css('width', '40%');
+                        quick_panel.css('width', '70%');
                         quick_panel.removeClass('d-none');
                         $("html").addClass("side-panel-overlay");
                         $(".offcanvas-wrapper").html(response);
