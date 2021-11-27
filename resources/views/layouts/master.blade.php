a4e451552bcbcd5e95025e0028cfc710e3ec15c7
theeMahmud <mahmudrajucse@gmail.com>
2021-11-27 12:01:18 +0600

    office_name

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,7 +47,7 @@
                 </button>
                 <!--end::Toolbar-->
             </div>
-            <h4 class="text-center"> {{$userOffices[0]['office_name_bn']}}</h4>
+            <h4 class="text-center"> {{session('_current_office')['office_name_bn']}}</h4>
             <!--end::Brand-->
             @yield('sideMenu')
         </div>
