321775a2c28205ead2642b2ac0b7600cf9f2944f
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2021-12-19 12:18:28 +0600

    audit menu bug

--- resources/views/layouts/partials/_sidenav.blade.php
+++ resources/views/layouts/partials/_sidenav.blade.php
@@ -1,14 +1,14 @@
 <!--begin::Aside Menu-->
 <div class="aside-menu-wrapper flex-column-fluid" id="kt_aside_menu_wrapper">
-    <x-menu-module-name>{{session('_module_menus')['module']['module_name_en']}}</x-menu-module-name>
+    <x-menu-module-name>{{session('_module_menus')['module']['title_en']}}</x-menu-module-name>
     <div id="kt_aside_menu" class="aside-menu my-4" data-menu-vertical="1" data-menu-scroll="1"
          data-menu-dropdown-timeout="500">
         <ul class="menu-nav">
             {{--menu-item-active--}}
             @foreach(session('_module_menus')['menus'] as $menu)
-                <x-menu-item class="{{$menu['menu_class']}}" href="{{url($menu['menu_link'])}}"
-                             icon="{{$menu['menu_icon']}}">
-                    {{$menu['menu_name_en']}}
+                <x-menu-item class="{{$menu['class']}}" href="{{url($menu['link'])}}"
+                             icon="{{$menu['icon']}}">
+                    {{$menu['title_en']}}
                 </x-menu-item>
             @endforeach
         </ul>
