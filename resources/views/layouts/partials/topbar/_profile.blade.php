8384805a539e881e3eda2757464633fa01fa3694
theeMahmud <mahmudrajucse@gmail.com>
2022-01-04 15:50:36 +0600

    profile

--- resources/views/layouts/partials/topbar/_profile.blade.php
+++ resources/views/layouts/partials/topbar/_profile.blade.php
@@ -8,7 +8,7 @@
             @if(isset($userDetails['user_role_id']) && $userDetails['user_role_id'] == 1)
                 {{__('Superman')}}
             @elseif(isset($userDetails['user_role_id']) && $userDetails['user_role_id'] == 3)
-                {{ $employeeInfo['name_bng'] ?? 'User Name' }}
+                {{ $employeeInfo['name_bng'] ?? 'User Name' }} {{isset($userOffices) ? ', ' . $userOffices[0]['designation'] : ''}}
             @endif
         </span>
         <span><i class="fa fa-chevron-down"></i></span>
@@ -30,7 +30,7 @@
                             <a href="{{route('change.office', [$office['id'], $office['office_id'], $office['office_unit_id'], $office['office_unit_organogram_id']])}}"
                                class="btn-switch-designation flex-fill overflow-hidden">
                                 <span>{{ $office['designation'] }}, </span>
-{{--                                <span class="test text-truncate">{{ $office['unit_name_bn'] }}</span>--}}
+                                {{--                                <span class="test text-truncate">{{ $office['unit_name_bn'] }}</span>--}}
                                 <span class="test text-truncate">{{ $office['office_name_bn'] }}</span>
                             </a>
                         @empty
