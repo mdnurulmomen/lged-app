afa32f05834cd23a404a8752a25bf02ee212ba49
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2022-02-09 12:04:43 +0600

    qac 2 bug

--- resources/views/scripts/audit_plan/edit/script_edit_entity_audit_plan.blade.php
+++ resources/views/scripts/audit_plan/edit/script_edit_entity_audit_plan.blade.php
@@ -63,7 +63,7 @@
         branding: false,
         content_style: "body {font-family: solaimanlipi;font-size: 13pt;}",
         fontsize_formats: "8pt 10pt 12pt 13pt 14pt 18pt 24pt 36pt",
-        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Oswald=oswald; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Times New Roman=times new roman,times; Verdana=verdana,geneva; Solaimanlipi=solaimanlipi",
+        font_formats: "Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Oswald=oswald; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Times New Roman=times new roman,times; Verdana=verdana,geneva; Solaimanlipi=solaimanlipi; AdorshoLipi=adorshoLipi",
         toolbar: ['styleselect fontselect fontsizeselect | blockquote subscript superscript | undo redo | cut copy paste | bold italic | link image | alignleft aligncenter alignright alignjustify ',
             'table | bullist numlist | outdent indent | advlist | autolink | lists charmap | print preview |  code'],
         plugins: 'advlist paste autolink link image lists charmap print preview code table',
