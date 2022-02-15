ec414d72a7fc83a578751b0599f6fd614cd47723
Robin <faisalrobin22@gmail.com>
2022-02-15 19:48:29 +0600

    label change

--- resources/views/modules/audit_quality_control/qac_apotti_list.blade.php
+++ resources/views/modules/audit_quality_control/qac_apotti_list.blade.php
@@ -49,11 +49,11 @@
                                 data-air-report-name="{{$responseData['rAirInfo']['report_name']}}"
                                 class="btn btn-sm btn-square btn-primary btn-hover-primary air_sent_responsible_party"
                                 onclick="QAC_Apotti_List_Container.airSendToRpu($(this))">
-                            <i class="fad fa-paper-plane"></i> রেস্পন্সিবল বরাবর প্রেরণ করুন
+                            <i class="fad fa-paper-plane"></i> রেস্পন্সিবল পার্টি বরাবর প্রেরণ করুন
                         </button>
                     @elseif($responseData['rAirInfo']['r_air_child']['is_received']== null)
                         <span class="badge badge-primary">
-                          <i class="fal fa-info text-white"></i>  রেস্পন্সিবল বরাবর প্রেরণ করা হয়েছে
+                          <i class="fal fa-info text-white"></i>  রেস্পন্সিবল পার্টি বরাবর প্রেরণ করা হয়েছে
                         </span>
                     @elseif($responseData['rAirInfo']['r_air_child']['is_received']== 1)
                         <span class="badge badge-primary">
