ebe05c9e5c0e74ec89c7cdb4287e6eac01bee48a
theeMahmud <mahmudrajucse@gmail.com>
2022-02-24 20:32:30 +0600

    footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,12 +3,12 @@
     <div class="container-fluid d-flex flex-column flex-md-row align-items-center justify-content-between py-4">
         <!--begin::Copyright-->
         <div class="text-dark">
-            <span class="text-dark font-weight-bold">&copy; Comptroller and Auditor General of Bangladesh</span>
+            <span class="text-dark font-weight-bold">&copy;Comptroller and Auditor General of Bangladesh</span>
         </div>
         <!--end::Copyright-->
         <!--end::Nav-->
         <div class="text-dark">
-            <img src="{{ asset('assets/images/tapp_logo.png') }}" alt="tappware"> Develop By <a href="https://tappware.com/">Tappware</a>
+            <img src="{{ asset('assets/images/tapp_logo.png') }}" alt="tappware"> Developed By <a href="https://tappware.com/">Tappware Solutions Limited</a>
         </div>
         <!--end::Copyright-->
 
