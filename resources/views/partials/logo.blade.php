0223967dc5f3dfc6fa8c36e1a8cc76322b844998
theeMahmud <mahmudrajucse@gmail.com>
2022-03-02 21:34:25 +0600

    design !!

--- resources/views/partials/logo.blade.php
+++ resources/views/partials/logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" class="brand-logo d-flex align-items-center" style="{{$area_style ?? ''}}">
     <img alt="Logo" style="{{$img_style ?? ''}}" src="{{ asset('assets/images/logo.png') }}"/>
-    <h2 class="mb-0 ml-4 text-dark font-weight-bolder">{{$text ?? 'AMMS 2.0'}}</h2>
+    <h2 class="mb-0 ml-4 text-dark font-weight-bolder text-white">{{$text ?? 'AMMS 2.0'}}</h2>
 </a>
