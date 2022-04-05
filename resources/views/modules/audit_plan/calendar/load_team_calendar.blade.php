de07f81179065b08fb5042c23854526c9088dcc3
theeMahmud <mahmudrajucse@gmail.com>
2022-04-05 11:47:35 +0600

    c

--- resources/views/modules/audit_plan/calendar/load_team_calendar.blade.php
+++ resources/views/modules/audit_plan/calendar/load_team_calendar.blade.php
@@ -1,7 +1,2 @@
-<style>
-    .fc-unthemed .fc-event .fc-content, .fc-unthemed .fc-event-dot .fc-content {
-        padding-left: 0.55rem !important;
-    }
-</style>
 <div id="kt_calendar"></div>
 @include('scripts.script_team_visit_plan_calendar')
