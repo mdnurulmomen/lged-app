769badacbe4bf505a29f1ba17af0a4080c399bae
Shamim kafi <shamimunbindelowarkafi@gmail.com>
2022-05-29 18:59:57 +0600

    audit archive report entity

--- resources/views/modules/audit_execution/audit_execution_archive_apotti_report/index.blade.php
+++ resources/views/modules/audit_execution/audit_execution_archive_apotti_report/index.blade.php
@@ -28,6 +28,13 @@
                 </select>
             </div>
 
+            <div class="col-md-3">
+                <label for="entity_id" class="col-form-label">এনটিটি</label>
+                <select class="form-select select-select2" id="entity_id" name="entity_id">
+                    <option value="">সবগুলো</option>
+                </select>
+            </div>
+
             <div class="col-md-2">
                 <label for="audit_year_start" class="col-form-label">আপত্তির অর্থবছর</label>
                 <div class="input-group">
@@ -35,8 +42,9 @@
                     <input class="form-control year-picker" name="year_to" placeholder="শেষ" type="text">
                 </div>
             </div>
-
-            <div class="mt-12 col-md-3">
+        </div>
+        <div class="mt-2 row">
+            <div class="col-md-3">
                 <button onclick="Archive_Apotti_Report_Container.load_apotti_report_list()"
                         class="btn btn-sm btn-primary btn-square" type="button">
                     <i class="fad fa-search"></i> অনুসন্ধান
@@ -63,10 +71,11 @@
         load_apotti_report_list: function (page = 1, per_page = 10) {
             directorate_id = $("#directorate_id").val();
             ministry_id = $("#ministry_id").val();
+            entity_id = $("#entity_id").val();
             year_from = $("#year_from").val();
             year_to = $("#year_to").val();
             let url = '{{route('audit.execution.archive-apotti-report.list')}}';
-            let data = {directorate_id, ministry_id, year_from, year_to, page, per_page};
+            let data = {directorate_id, ministry_id, entity_id, year_from, year_to, page, per_page};
 
             KTApp.block('#kt_content', {
                 opacity: 0.1,
@@ -177,6 +186,19 @@
             );
         },
 
+        loadMinistryWiseEntity: function (ministry_id) {
+            let url = '{{route('audit.execution.archive-apotti.load-ministry-wise-entity')}}';
+            let data = {ministry_id};
+            ajaxCallAsyncCallbackAPI(url, data, 'POST', function (response) {
+                    if (response.status === 'error') {
+                        toastr.warning(response.data);
+                    } else {
+                        $('#entity_id').html(response);
+                    }
+                }
+            );
+        },
+
         apotti_sync: function (elem) {
             report_id =  elem.data('report-id');
             let url = '{{route('audit.execution.archive-apotti-report.report-sync')}}';
@@ -203,5 +225,11 @@
     $('#directorate_id').change(function () {
         directorate_id = $('#directorate_id').val();
         Archive_Apotti_Container.loadDirectorateWiseMinistry(directorate_id);
+    });
+
+    //entity
+    $('#ministry_id').change(function() {
+        ministry_id = $('#ministry_id').val();
+        Archive_Apotti_Common_Container.loadMinistryWiseEntity(ministry_id);
     });
 </script>
