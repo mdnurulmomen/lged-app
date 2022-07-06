3153dc87cc9e6a1919574e08d06c15740cbb419a
Robin <faisalrobin22@gmail.com>
2022-07-06 12:59:04 +0600

    annual plan current fiscal year temp

--- resources/views/modules/audit_plan/annual/annual_plan_revised/annual_plan_lists.blade.php
+++ resources/views/modules/audit_plan/annual/annual_plan_revised/annual_plan_lists.blade.php
@@ -7,7 +7,7 @@
                 <option value="">--সিলেক্ট--</option>
                 @foreach($fiscal_years as $fiscal_year)
                     <option
-                        value="{{$fiscal_year['id']}}" {{now()->year == $fiscal_year['end']?'selected':''}}>{{$fiscal_year['description']}}</option>
+                        value="{{$fiscal_year['id']}}" {{now()->year + 1 == $fiscal_year['end']?'selected':''}}>{{$fiscal_year['description']}}</option>
                 @endforeach
             </select>
         </div>
