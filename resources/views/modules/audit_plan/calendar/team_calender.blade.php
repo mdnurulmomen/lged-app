c80ab8c2aba73e57c87b880aa7a8b3b3e354e9a0
Robin <faisalrobin22@gmail.com>
2022-07-25 10:19:14 +0600

    label change dashboard

--- resources/views/modules/audit_plan/calendar/team_calender.blade.php
+++ resources/views/modules/audit_plan/calendar/team_calender.blade.php
@@ -71,7 +71,7 @@
         </a>
     </div>
     <div class="d-flex flex-wrap text-center w-50 justify-content-center pl-1">
-        <h4 class="w-100 py-3 m-0">As of Today</h4>
+        <h4 class="w-100 py-3 m-0">Till Today</h4>
         <a href="javascript:;" data-page="query" data-status="yearly"
            onclick="Team_Calendar_Container.pageRedirect($(this))" class="w-50 pt-3">
             <h6 class="font-weight-bold">Query</h6>
