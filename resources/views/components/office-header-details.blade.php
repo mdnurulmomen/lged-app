703316b7b1a0960e27776cbc739f407228bde375
DESKTOP-VUGJ8J1\DELL <shamimkafi123@gmail.com>
2022-07-31 17:56:31 +0600

    bug

--- resources/views/components/office-header-details.blade.php
+++ resources/views/components/office-header-details.blade.php
@@ -5,7 +5,7 @@
     <div class="bangla-font" style="font-family:Nikosh,serif !important;text-align: center">
         {{--মহাপরিচালকের কার্যালয় <br>--}}
         <b style="font-size: 20px">{{$office_name_bn}}</b> <br>
-        {!! $office_details['office_address'] !!}<br>
+        {!! nl2br($office_details['office_address']) !!}<br>
         <u>{{$office_details['office_web']}}</u>
     </div>
 @endif
