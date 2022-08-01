de49c7d46acdc9971733524cc2f592dde70a7005
DESKTOP-VUGJ8J1\DELL <shamimkafi123@gmail.com>
2022-08-01 15:57:29 +0600

    ডাটা কালেকশন

--- resources/views/modules/audit_plan/audit_plan/dc_office_order/partials/office_order_book_dc.blade.php
+++ resources/views/modules/audit_plan/audit_plan/dc_office_order/partials/office_order_book_dc.blade.php
@@ -877,7 +877,7 @@
                              <td class="bangla-font" style="text-align: center" width="5%">ক্রমিক নং</td>
                              <td class="bangla-font" style="text-align: center" width="45%">শাখার নাম</td>
                              <td class="bangla-font" style="text-align: center" width="20%">নিরীক্ষা বছর</td>
-                             <td class="bangla-font" style="text-align: center" width="15%">নিরীক্ষা সময়কাল</td>
+                             <td class="bangla-font" style="text-align: center" width="15%">ডাটা কালেকশন সময়কাল</td>
                              <td class="bangla-font" style="text-align: center" width="15%">মোট কর্ম দিবস</td>
                          </tr>
                          <tr>
--- resources/views/modules/audit_plan/audit_plan/dc_office_order/show_office_order_dc.blade.php
+++ resources/views/modules/audit_plan/audit_plan/dc_office_order/show_office_order_dc.blade.php
@@ -106,7 +106,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                        
+
 
                         @php $teamMemberSL = 1; @endphp
                         @foreach(json_decode($audit_team_schedule['team_members'],true) as $role => $team_members)
@@ -135,7 +135,7 @@
                             <td style="text-align: center" width="5%">ক্রমিক নং</td>
                             <td style="text-align: center" width="45%">শাখার নাম</td>
                             <td style="text-align: center" width="20%">নিরীক্ষা বছর</td>
-                            <td style="text-align: center" width="15%">নিরীক্ষা সময়কাল</td>
+                            <td style="text-align: center" width="15%">ডাটা কালেকশন সময়কাল</td>
                             <td style="text-align: center" width="15%">মোট কর্ম দিবস</td>
                         </tr>
                         <tr>
