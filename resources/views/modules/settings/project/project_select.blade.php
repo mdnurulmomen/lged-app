410a85082271d4d65eec383d60a109dd2710d2e6
Robin <faisalrobin22@gmail.com>
2022-11-21 15:42:44 +0600

    project select

--- resources/views/modules/settings/project/project_select.blade.php
+++ resources/views/modules/settings/project/project_select.blade.php
@@ -1,5 +1,5 @@
 <option value="">সবগুলো</option>
 @foreach ($all_project as $project)
-    <option data-name-en="{{ $project['name_en'] }}" data-name-bn="{{ $project['name_en'] }}"
+    <option data-name-en="{{ $project['name_en'] }}" data-name-bn="{{ $project['name_bn'] }}"
             value="{{ $project['id'] }}">{{ $project['name_bn'] }}</option>
 @endforeach
