48fb276923f8f0efa3a7cea687d6554f84b90015
Miftahur23 <miftahur109970@gmail.com>
2023-01-08 12:26:00 +0600

    objective and scope fethced

--- resources/views/modules/individual_plan/partial/announcement-memo-modal.blade.php
+++ resources/views/modules/individual_plan/partial/announcement-memo-modal.blade.php
@@ -1,9 +1,12 @@
-<button class="btn btn-sm btn-square btn-primary btn-hover-primary"
-    data-audit-plan-id="{{$announcementMemo['engagement_letter']['audit_plan_id']}}"
-    data-yearly_plan_location_id="{{$announcementMemo['audit_plan_info']['yearly_plan_location_id']}}"
-    onclick="Individual_Plan_Container.downloadAnnouncementModal($(this))">
-    Download
-</button>
+<div style="text-align: right;">
+    <button class="btn btn-download btn-sm btn-bold btn-square"
+        data-audit-plan-id="{{$announcementMemo['audit_plan_id']}}"
+        data-yearly_plan_location_id="{{$announcementMemo['audit_plan']['yearly_plan_location']['id']}}"
+        onclick="Individual_Plan_Container.downloadAnnouncementModal($(this))">
+        <i class="far fa-file-pdf"></i> Download
+    </button>
+</div>
+
 <div class="card sna-card-border mt-3" style="margin-bottom:30px; padding: 25px;">
     <div class="col-sm-12">
         <b>Ref : </b>
@@ -11,7 +14,7 @@
 
     <div class="col-sm-12">
     <?php
-        $date = \Carbon\Carbon::parse($announcementMemo['engagement_letter']['created_at'])->format('d/m/Y');
+        $date = \Carbon\Carbon::parse($announcementMemo['created_at'])->format('d/m/Y');
     ?>
         <b>Date : {{$date}}</b>
     </div>
@@ -21,46 +24,46 @@
     </div>
 
     <div class="col-sm-12 mt-3" style="text-align: left;">
-        {{$announcementMemo['engagement_letter']['letter_to']}}
+        {{$announcementMemo['letter_to']}}
         <br>
-        {{$announcementMemo['yearly_plan_info']['project_name_en']}}
+        {{$announcementMemo['audit_plan']['yearly_plan_location']['project_name_en']}}
         <br>
         LGED, Dhaka.
     </div>
 
 
     <div class="col-sm-12 mt-3">
-        <b>Subject: {{$announcementMemo['engagement_letter']['subject']}}</b>
+        <b>Subject: {{$announcementMemo['subject']}}</b>
     </div>
 
     <div class="col-sm-12 mt-2">
-        <span>Reference : Memo No - {{$announcementMemo['office_order']['memorandum_no']}}</span>
+        <span>Reference : Memo No - {{$announcementMemo['audit_plan']['office_order']['memorandum_no']}}</span>
     </div>
 
     <div class="col-sm-12 mt-2" style="text-align: justify;">
-        {{$announcementMemo['engagement_letter']['body']}}
+        {{$announcementMemo['body']}}
     </div>
 
     <div class="col-sm-12 mt-2" style="text-align: justify;">
        <b>OBJECTIVE OF THE AUDIT</b>
        <br>
-       {!!$announcementMemo['audit_plan_info']['objective']!!}
+       {!!$announcementMemo['audit_plan']['objective']!!}
     </div>
 
     <div class="col-sm-12 mt-2" style="text-align: justify;">
        <b>SCOPE OF THE AUDIT</b>
        <br>
-       {!!$announcementMemo['audit_plan_info']['scope']!!}
+       {!!$announcementMemo['audit_plan']['scope']!!}
     </div>
 
     <div class="col-sm-12 mt-2" style="text-align: justify;">
-        {!!$announcementMemo['engagement_letter']['others']!!}
+        {!!$announcementMemo['others']!!}
     </div>
 
     <div class="col-sm-12 mt-2" style="text-align: justify;">
         Yours sincerely,
         <br>
-        {!!$announcementMemo['engagement_letter']['letter_from']!!}
+        {!!$announcementMemo['letter_from']!!}
         <br>
         LGED, Dhaka.
         <br>
